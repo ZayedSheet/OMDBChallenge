7ae7f293cc4e5ad0daa2660b7e3868f1d251c2ea
Zayed Sheet <zayed@Zayeds-MacBook-Pro.local>
2021-01-17 18:45:21 -0500

    added local storage so nomination state persists

--- src/DataStore/DataProvider.js
+++ src/DataStore/DataProvider.js
@@ -1,4 +1,4 @@
-import React, { createContext, useContext, useState } from 'react';
+import React, { createContext, useContext, useState, useEffect } from 'react';
 const Context = createContext();
 
 export const useDataProvider = () => useContext(Context);
@@ -7,7 +7,16 @@ export default ({ children }) => {
   const [nominations, setNominations] = useState([]);
   const [searchResults, setSearchResults] = useState([]);
   const [search, setSearch] = useState("");
-  
+
+  useEffect(() => {
+    const savedState = localStorage.getItem('saveState');
+    console.log(savedState);
+    if(savedState){setNominations(JSON.parse(savedState))};
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('saveState', JSON.stringify(nominations));
+  }, [nominations]);
 
   const store = {
     searchStore: {
