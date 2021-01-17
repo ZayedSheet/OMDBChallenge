a85c71a135fa99c4edef63531a63118c3e809bb9
Zayed Sheet <zayed@Zayeds-MacBook-Pro.local>
2021-01-17 17:45:13 -0500

    changed app to render all componenents

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
 import {AppProvider} from "@shopify/polaris";
 import en from '@shopify/polaris/locales/en.json';
+import AppBar from 'components/AppBar/app-bar';
+import SearchBar from 'components/Search/search-bar';
+import ResultsList from 'components/MovieList/results-list';
+import NominationList from 'components/MovieList/nomination-list'
 
 import DataProvider from 'DataStore/DataProvider';
 
@@ -12,6 +16,12 @@ function App() {
     <DataProvider>
       <AppProvider i18n={en}>
         <div className="App">
+          <AppBar></AppBar>
+          <div className="app-body">
+            <SearchBar></SearchBar>
+            <ResultsList></ResultsList>
+            <NominationList></NominationList>
+          </div>
         </div>
       </AppProvider>
     </DataProvider>
