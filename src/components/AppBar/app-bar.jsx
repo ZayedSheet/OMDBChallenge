2f18e8424e645d240d1e045767b0a106fd6c4b41
Zayed Sheet <zayed@Zayeds-MacBook-Pro.local>
2021-01-17 17:44:31 -0500

    added  app bar

---
+++ src/components/AppBar/app-bar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./app-bar.css";
+
+export default function AppBar() {
+
+  return (
+    <div id="app-bar">
+      <h1>
+        The Shoppies
+      </h1>
+    </div>
+  );
+}
