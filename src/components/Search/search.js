cc7a62db1a05749fb4ef9bba81d47ca500c07ab7
Zayed Sheet <zayed@Zayeds-MacBook-Pro.local>
2021-01-17 19:27:55 -0500

    changed api request link to https

--- src/components/Search/search.js
+++ src/components/Search/search.js
@@ -4,7 +4,7 @@ const APIKEY = "1ae68295";
 
 export default async function requestSearch(search) {
   try{
-    const movies = await axios.get(`http://www.omdbapi.com/?s=${search}&apikey=${APIKEY}`);
+    const movies = await axios.get(`https://www.omdbapi.com/?s=${search}&apikey=${APIKEY}`);
     return movies;
   }catch(e){
     console.log("error retreiving info ", e);
